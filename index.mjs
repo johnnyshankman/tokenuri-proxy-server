133f7238007800c6524a8ce48435cacbacdcc815
Johnny Shankman <johnnyshankman@users.noreply.github.com>
2023-06-22 09:59:59 -0400

    better variable names

--- index.mjs
+++ index.mjs
@@ -34,7 +34,7 @@ app.get('/:contractaddress/:id', async (req, res) => {
 
   const functionSignatureHash = '0xc87b56dd' // Keccak-256 hash of "tokenURI(uint256)" and take the first 4 bytes
   const encodedArgs = ethers.utils.defaultAbiCoder.encode(['uint256'], [id])
-  const d = functionSignatureHash + encodedArgs.substring(2) // Remove the "0x" prefix from the encoded arguments
+  const functionData = functionSignatureHash + encodedArgs.substring(2) // Remove the "0x" prefix from the encoded arguments
   try {
     const response = await fetch('https://cloudflare-eth.com/', {
       method: 'POST',
@@ -48,23 +48,21 @@ app.get('/:contractaddress/:id', async (req, res) => {
         params: [
           {
             to: ethers.utils.getAddress(contractaddress),
-            data: d,
+            data: functionData,
           },
           'latest',
         ],
       }),
     })
-    const data = await response.json()
-    const result = data.result
-    const ressy = ethers.utils.toUtf8String(result)
+    const responseJSONFromRPC = await response.json()
+    const decodedResult = ethers.utils.toUtf8String(responseJSONFromRPC.result)
     // @dev: this is not flexible at all and is specific to on-chain data:application URLs
     // @dev: it solves for the fact that ressy/result are left-padded with 0s when returning strings
-    const indexy = ressy.indexOf('data:application')
-    // cut out all text before the data:application occurence
-    let theactualJSON = ressy.substring(indexy)
-    theactualJSON = await fetch(theactualJSON)
-    theactualJSON = await theactualJSON.json()
-    res.json(theactualJSON)
+    const trimIndex = decodedResult.indexOf('data:application')
+    let urlOfNFTJSON = ressy.substring(trimIndex) // cut out all text before the data:application occurence
+    const theNFTJSONResponse = await fetch(urlOfNFTJSON)
+    const theNFTJSON = await theactualJSONResponse.json()
+    res.json(theNFTJSON)
   } catch(e) {
     res.status(500).send(e)
   }
